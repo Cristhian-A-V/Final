2751cef6b603a14634800716ae9bb238d0866b83
Cristhian Arispe Vargas <a2019114216@estudiantes.upsa.edu.bo>
2020-12-12 11:42:48 -0400

    Se creo el controlador

---
+++ Final/Final/Controllers/FinalController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Final.Controllers
+{
+    public class FinalController : ApiController
+    {
+        public string operaciones(int numero) 
+        {
+            string mensaje;
+            if (numero<0)
+            {
+                mensaje = "ERROR";
+            }
+            else
+            {
+                if (numero==0)
+                {
+                    mensaje = "Realizado por Cristhian Arispe Vargas";
+                }
+                else
+                {
+                    mensaje = "https://image.freepik.com/vector-gratis/numeros-cartel-imagen_1639-6453.jpg";
+                }
+            }
+            return mensaje;
+        }
+    }
+}
